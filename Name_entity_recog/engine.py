61f0c841b01c3351404bf59cee9cf18d39120f86
Dungdeptrai <@mail.com>
2024-08-29 00:17:28 +0700

    still having cuda error :v

--- Name_entity_recog/engine.py
+++ Name_entity_recog/engine.py
@@ -45,7 +45,7 @@ def train_step(model: torch.nn.Module,
         y_pred = model(X)
 
         # 2. Calculate  and accumulate loss
-        loss = loss_fn(y_pred, y)
+        loss = loss_fn(y_pred.permute(0,2,1), y)
         train_loss += loss.item() 
 
         # 3. Optimizer zero grad
@@ -58,12 +58,9 @@ def train_step(model: torch.nn.Module,
         optimizer.step()
 
         # Calculate and accumulate accuracy metric across all batches
-        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
-        train_acc += (y_pred_class == y).sum().item()/len(y_pred)
 
     # Adjust metrics to get average loss and accuracy per batch 
     train_loss = train_loss / len(dataloader)
-    train_acc = train_acc / len(dataloader)
     return train_loss, train_acc
 
 def test_step(model: torch.nn.Module, 
@@ -104,16 +101,13 @@ def test_step(model: torch.nn.Module,
             test_pred_logits = model(X)
 
             # 2. Calculate and accumulate loss
-            loss = loss_fn(test_pred_logits, y)
+            loss = loss_fn(test_pred_logits.permute(0,2,1), y)
             test_loss += loss.item()
 
             # Calculate and accumulate accuracy
-            test_pred_labels = test_pred_logits.argmax(dim=1)
-            test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
 
     # Adjust metrics to get average loss and accuracy per batch 
     test_loss = test_loss / len(dataloader)
-    test_acc = test_acc / len(dataloader)
     return test_loss, test_acc
 
 def train(model: torch.nn.Module, 
